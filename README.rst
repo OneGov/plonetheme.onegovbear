e48ca93582207ad08f86fd0822b252f53c70f8e3
Jonas Baumann <jone@jone.ch>
2015-06-11 10:55:27 +0200

    bootstrap package.

---
+++ README.rst
@@ -0,0 +1,44 @@
+plonetheme.onegovbear
+=====================
+
+This is a Plone theme optimized for use with OneGov module  (http://onegov.ch).
+
+Thi theme is based on `ftw.theming`_.
+
+
+Installation
+------------
+
+Add ``plonetheme.onegovbear`` to your buildout configuration
+
+::
+
+    [instance]
+    eggs +=
+        plonetheme.onegovbear
+
+or as dependency to the ``setup.py`` of your policy package.
+
+After rerunning buildout install the Generic Setup profile.
+
+
+
+
+Links
+-----
+
+- Github project repository: https://github.com/OneGov/plonetheme.onegovbear
+- Issue tracker: https://github.com/OneGov/plonetheme.onegovbear/issues
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=plonetheme.onegovbear
+
+
+
+Copyright
+---------
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``plonetheme.onegovbear`` is licensed under GNU General Public License, version 2.
+
+
+.. _ftw.theming: https://github.com/4teamwork/ftw.theming
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0.dev0'
+
+setup(name='plonetheme.onegovbear',
+      version=version,
+      description='OneGov Plone theme sponsered by the city of Bern.',
+      long_description=(open('README.rst').read() + '\n' +
+                        open(os.path.join('docs', 'HISTORY.txt')).read()),
+
+      classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ],
+
+      keywords='plone theme onegov',
+      author='4teamwork AG',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/OneGov/plonetheme.onegovbear',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'plone.app.theming',
+        ],
+
+      entry_points='''
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
+      )
