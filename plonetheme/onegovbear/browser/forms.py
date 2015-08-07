bd5503933eddbaf016f91dfcc11ab6557b7bb088
Martin Bächtold <github@tph.ch>
2015-08-07 11:28:29 +0200

    Refactor code to make it more readable.

--- plonetheme/onegovbear/browser/forms.py
+++ plonetheme/onegovbear/browser/forms.py
@@ -10,72 +10,39 @@ ANNOTATION_KEY = 'plonetheme.onegovebear.custom_scss_variables'
 
 
 class IVariablesSchema(form.Schema):
-    primary_color = Variable(
-        variable_name='$primary-color')
-    secondary_color = Variable(
-        variable_name='$secondary-color')
-    globalnav_bg_color = Variable(
-        variable_name='$globalnav-bg-color')
-    globalnav_bg_color_hover = Variable(
-        variable_name='$globalnav-bg-color-hover')
-    globalnav_link_color = Variable(
-        variable_name='$globalnav-link-color')
-    globalnav_link_color_hover = Variable(
-        variable_name='$globalnav-link-color-hover')
-    globalnav_separator_color = Variable(
-        variable_name='$globalnav-separator-color')
-    button_standalone_color = Variable(
-        variable_name='$button-standalone-color')
-    button_context_color = Variable(
-        variable_name='$button-context-color')
-    button_destructive_color = Variable(
-        variable_name='$button-destructive-color')
-    button_success_color = Variable(
-        variable_name='$button-success-color')
-    button_warning_color = Variable(
-        variable_name='$button-warning-color')
-    button_danger_color = Variable(
-        variable_name='$button-danger-color')
-    button_disabled_color = Variable(
-        variable_name='$button-disabled-color')
-    button_text_light_color = Variable(
-        variable_name='$button-text-light-color')
-    button_text_dark_color = Variable(
-        variable_name='$button-text-dark-color')
-    footer_background_color = Variable(
-        variable_name='$footer-background-color')
-    footer_font_color = Variable(
-        variable_name='$footer-font-color')
-    footer_link_color = Variable(
-        variable_name='$footer-link-color')
-    footer_link_color_hover = Variable(
-        variable_name='$footer-link-color-hover')
-    font_family_base = Variable(
-        variable_name='$font-family-base')
-    line_height_base = Variable(
-        variable_name='$line-height-base')
-    font_size_base = Variable(
-        variable_name='$font-size-base')
-    font_size_h1 = Variable(
-        variable_name='$font-size-h1')
-    font_size_h2 = Variable(
-        variable_name='$font-size-h2')
-    font_size_h3 = Variable(
-        variable_name='$font-size-h3')
-    page_bg_color = Variable(
-        variable_name='$page-bg-color')
-    gray_base = Variable(
-        variable_name='$gray-base')
-    gray_darker = Variable(
-        variable_name='$gray-darker')
-    gray_dark = Variable(
-        variable_name='$gray-dark')
-    gray = Variable(
-        variable_name='$gray')
-    gray_light = Variable(
-        variable_name='$gray-light')
-    gray_lighter = Variable(
-        variable_name='$gray-lighter')
+    primary_color = Variable('$primary-color')
+    secondary_color = Variable('$secondary-color')
+    globalnav_bg_color = Variable('$globalnav-bg-color')
+    globalnav_bg_color_hover = Variable('$globalnav-bg-color-hover')
+    globalnav_link_color = Variable('$globalnav-link-color')
+    globalnav_link_color_hover = Variable('$globalnav-link-color-hover')
+    globalnav_separator_color = Variable('$globalnav-separator-color')
+    button_standalone_color = Variable('$button-standalone-color')
+    button_context_color = Variable('$button-context-color')
+    button_destructive_color = Variable('$button-destructive-color')
+    button_success_color = Variable('$button-success-color')
+    button_warning_color = Variable('$button-warning-color')
+    button_danger_color = Variable('$button-danger-color')
+    button_disabled_color = Variable('$button-disabled-color')
+    button_text_light_color = Variable('$button-text-light-color')
+    button_text_dark_color = Variable('$button-text-dark-color')
+    footer_background_color = Variable('$footer-background-color')
+    footer_font_color = Variable('$footer-font-color')
+    footer_link_color = Variable('$footer-link-color')
+    footer_link_color_hover = Variable('$footer-link-color-hover')
+    font_family_base = Variable('$font-family-base')
+    line_height_base = Variable('$line-height-base')
+    font_size_base = Variable('$font-size-base')
+    font_size_h1 = Variable('$font-size-h1')
+    font_size_h2 = Variable('$font-size-h2')
+    font_size_h3 = Variable('$font-size-h3')
+    page_bg_color = Variable('$page-bg-color')
+    gray_base = Variable('$gray-base')
+    gray_darker = Variable('$gray-darker')
+    gray_dark = Variable('$gray-dark')
+    gray = Variable('$gray')
+    gray_light = Variable('$gray-light')
+    gray_lighter = Variable('$gray-lighter')
 
 
 class VariablesConfig(object):
@@ -134,14 +101,17 @@ class VariablesForm(form.SchemaEditForm):
         if self.annotation_key not in annotations:
             annotations[self.annotation_key] = PersistentMapping()
 
-        # In case a field has been removed from the schema which previously
+        # In case a field is removed from the schema which previously
         # has been used to override a SCSS variable its value becomes stale.
         # The stale value needs to be removed from the annotations
         # since it cannot be removed in the form due to the missing form
-        # field.
-        for key in [key for key in annotations[self.annotation_key]]:
-            if key not in self.fields.keys():
-                del annotations[self.annotation_key][key]
+        # field. This is done by rebuilding the value stored in the
+        # annotations.
+        cleaned = {}
+        for key in self.fields.keys():
+            if key in annotations[self.annotation_key].keys():
+                cleaned[key] = annotations[self.annotation_key][key]
+        annotations[self.annotation_key] = cleaned
 
         return self.config(annotations[self.annotation_key], self.fields)
 
