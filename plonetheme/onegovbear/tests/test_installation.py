42210c957a51b9cca2547420d046ba7cf38c5581
Martin Bächtold <github@tph.ch>
2015-08-12 14:58:28 +0200

    Fix typo.

--- plonetheme/onegovbear/tests/test_installation.py
+++ plonetheme/onegovbear/tests/test_installation.py
@@ -7,4 +7,4 @@ class TestInstallation(FunctionalTestCase):
     def test_generic_setup_profile_is_installed(self):
         portal_setup = getToolByName(self.layer['portal'], 'portal_setup')
         version = portal_setup.getLastVersionForProfile('plonetheme.onegovbear:default')
-        self.assertNotEquals('unkown', version)
+        self.assertNotEquals('unknown', version)
