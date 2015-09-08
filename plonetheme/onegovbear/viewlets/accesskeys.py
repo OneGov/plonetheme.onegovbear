efcfd5befe3b945912a52e39750750a934525f65
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2015-09-08 09:21:07 +0200

    Add contact accesskey.

--- plonetheme/onegovbear/viewlets/accesskeys.py
+++ plonetheme/onegovbear/viewlets/accesskeys.py
@@ -1,6 +1,12 @@
+from plone import api
 from plone.app.layout.viewlets import common
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class OneGovBearSkipLinksViewlet(common.SkipLinksViewlet):
     index = ViewPageTemplateFile('accesskeys.pt')
+
+    def update(self):
+        super(OneGovBearSkipLinksViewlet, self).update()
+        self.nav_root_url = api.portal.get_navigation_root(
+            self.context).absolute_url()
