ca00ea0888a23c367249814d782c86ff53229aab
Kevin Bieri <k.bieri@4teamwork.ch>
2016-01-06 13:38:12 +0100

    Remove custom pathbar styling.
    Remove

--- plonetheme/onegovbear/viewlets/pathbar.py
+++
@@ -1,9 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets import common
-
-
-class PathBarViewlet(common.PathBarViewlet):
-    pathbar = ViewPageTemplateFile('pathbar.pt')
-
-    def index(self):
-        return self.pathbar()
