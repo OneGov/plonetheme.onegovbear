1d3c48349cf7a35d7dd1c6c8e150f0b5667adafb
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2016-06-22 08:14:38 +0200

    Show subsite title in pathbar instead of Home on subsites.

--- plonetheme/onegovbear/viewlets/pathbar.py
+++ plonetheme/onegovbear/viewlets/pathbar.py
@@ -1,5 +1,7 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ftw.subsite.interfaces import ISubsite
+from plone import api
 from plone.app.layout.viewlets import common
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class PathBarViewlet(common.PathBarViewlet):
@@ -7,3 +9,11 @@ class PathBarViewlet(common.PathBarViewlet):
 
     def index(self):
         return self.pathbar()
+
+    def get_subsite(self):
+        nav_root = api.portal.get_navigation_root(self.context)
+        if ISubsite.providedBy(nav_root):
+            return dict(title=nav_root.Title(),
+                        url=nav_root.absolute_url())
+        else:
+            return None
