a1e754aab5dd413500e1248f27316bae05f5b71f
raphael-s <r.schwob@4teamwork.ch>
2017-01-30 10:17:58 +0100

    Remove subsite dependency from pathbar viewlet

--- plonetheme/onegovbear/viewlets/pathbar.py
+++ plonetheme/onegovbear/viewlets/pathbar.py
@@ -1,5 +1,3 @@
-from ftw.subsite.interfaces import ISubsite
-from plone import api
 from plone.app.layout.viewlets import common
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -9,11 +7,3 @@ class PathBarViewlet(common.PathBarViewlet):
 
     def index(self):
         return self.pathbar()
-
-    def get_subsite(self):
-        nav_root = api.portal.get_navigation_root(self.context)
-        if ISubsite.providedBy(nav_root):
-            return dict(title=nav_root.Title(),
-                        url=nav_root.absolute_url())
-        else:
-            return None
